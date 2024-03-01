fc7731fc2ef806a9764bc2206a2301423ee0159b
rohan-sharma12 <rohanli3217@gmail.com>
2024-03-01 18:43:52 +0530

    Files added

--- 03.CustomReact/customReact.js
+++ 03.CustomReact/customReact.js
@@ -10,11 +10,25 @@ const reactElement = {  //How React sees each JSX element
 }  
 
 function customRenderer(reactElement, container){
+    
+    /* Version 1:
+    
     const DOMElement = document.createElement(reactElement.type); //Creating a DOM Element of a given type
     DOMElement.innerHTML = reactElement.chlidren; 
     DOMElement.setAttribute('href', reactElement.props.href); //Setting attributes of the DOM Element
     DOMElement.setAttribute('target', reactElement.props.target);
-    container.appendChild(DOMElement); //Injecting the created element into the DOM 
+    container.appendChild(DOMElement); //Injecting the created element into the DOM  
+    */
+
+
+    //Version 2:
+    const DOMElement = document.createElement(reactElement.type); 
+    DOMElement.innerHTML = reactElement.chlidren;
+    for(const prop in reactElement.props){
+    if(prop == 'chlidren') continue;
+    DOMElement.setAttribute(prop, reactElement.props[prop])    
+    }
+    container.appendChild(DOMElement);
 }
 
 customRenderer(reactElement, mainContainer) //Function to inject a react element into the DOM at a particular point
