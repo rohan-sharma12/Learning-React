df0465de60b0b775a4fd175e10c71dc40d7a5a31
rohan-sharma12 <rohanli3217@gmail.com>
2024-03-02 22:20:55 +0530

    Files added

--- 06.Tailwind And Props/src/App.jsx
+++ 06.Tailwind And Props/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className="text-100xl font-bold underline">
-      Hello world!
+    <h1 className="text-5xl font-bold">
+          Tailwind & Props
     </h1>
+    
+    <div className="flex items-center justify-center p-3 ">
+      <Card title="Lenovo Ideapad" description="8 RAM i7 10th Gen"/>
+      <Card title="Asus Vivobook"/>
+    </div>
     </>
-  )
+  );
 }
+
 
 export default App
